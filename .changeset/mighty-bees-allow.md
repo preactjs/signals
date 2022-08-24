812e7b5b3a6eac8be24353bdd12e61d3156370cb
Marvin Hagemeister <hello@marvinh.dev>
2022-08-24 22:48:42 +0200

    Fix missing batch() export in preact adapter

---
+++ .changeset/mighty-bees-allow.md
@@ -0,0 +1,5 @@
+---
+"@preact/signals": patch
+---
+
+Fix `batch()` not re-exported from preact adapter
---
+++ packages/preact/test/exports.test.ts
@@ -0,0 +1,16 @@
+import * as core from "@preact/signals-core";
+import * as adapter from "@preact/signals";
+
+describe("exports", () => {
+	it("should re-export core", () => {
+		const keys = Object.keys(core);
+
+		for (let i = 0; i < keys.length; i++) {
+			const key = keys[i];
+			expect(key in adapter).to.equal(
+				true,
+				`"${key}" is not exported from preact adapter`
+			);
+		}
+	});
+});
