7d12dd8edf86ceb588cfc2bd202d2afebb1d1797
Jason Miller <jason.miller@shopify.com>
2022-08-31 23:15:50 -0400

    Add @preact/signals-react package

---
+++ packages/react/src/index.ts
@@ -0,0 +1,153 @@
+import {
+	useRef,
+	useMemo,
+	createElement,
+	// @ts-ignore-next-line
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED as internals,
+} from "react";
+import React from "react";
+import {
+	signal,
+	computed,
+	batch,
+	effect,
+	Signal,
+	type ReadonlySignal,
+} from "@preact/signals-core";
+import { Updater, ReactOwner, ReactDispatcher } from "./internal";
+
+export { signal, computed, batch, effect, Signal, type ReadonlySignal };
+
+/**
+ * Install a middleware into React.createElement to replace any Signals in props with their value.
+ * @todo this likely needs to be duplicated for jsx()...
+ */
+// @ts-ignore-next-line
+React.createElement = function (type, props) {
+	if (typeof type === "string" && props) {
+		for (let i in props) {
+			let v = props[i];
+			if (i !== "children" && v instanceof Signal) {
+				// createPropUpdater(props, i, v);
+				props[i] = v.value;
+			}
+		}
+	}
+	// @ts-ignore-next-line
+	return createElement.apply(this, arguments);
+};
+
+/*
+// This breaks React's controlled components implementation
+function createPropUpdater(props: any, prop: string, signal: Signal) {
+	let ref = props.ref;
+	if (!ref) ref = props.ref = React.createRef();
+	effect(() => {
+		if (props) props[prop] = signal.value;
+		let el = ref.current;
+		if (!el) return; // unsubscribe
+		(el as any)[prop] = signal.value;
+	});
+	props = null;
+}
+*/
+
+let finishUpdate: ReturnType<Updater["_setCurrent"]> | undefined;
+const updaterForComponent = new WeakMap<ReactOwner, Updater>();
+
+function setCurrentUpdater(updater?: Updater) {
+	// end tracking for the current update:
+	if (finishUpdate) finishUpdate(true, true);
+	// start tracking the new update:
+	finishUpdate = updater && updater._setCurrent();
+}
+
+function createUpdater(updater: () => void) {
+	const s = signal(undefined) as Updater;
+	s._canActivate = true;
+	s._updater = updater;
+	return s;
+}
+
+/**
+ * A wrapper component that renders a Signal's value directly as a Text node.
+ */
+function Text({ data }: { data: Signal }) {
+	return data.value;
+}
+
+// Decorate Signals so React renders them as <Text> components.
+//@ts-ignore-next-line
+const $$typeof = createElement("a").$$typeof;
+Object.defineProperties(Signal.prototype, {
+	$$typeof: { value: $$typeof },
+	type: { value: Text },
+	props: {
+		get() {
+			return { data: this };
+		},
+	},
+	ref: { value: null },
+});
+
+// Track the current owner (roughly equiv to current vnode)
+// let currentOwner: ReactOwner;
+// Object.defineProperty(internals.ReactCurrentOwner, "current", {
+// 	get() { return currentOwner; },
+// 	set(owner) { currentOwner = owner; },
+// });
+
+// Track the current dispatcher (roughly equiv to current component impl)
+let lock = false;
+const UPDATE = () => ({});
+let currentDispatcher: ReactDispatcher;
+Object.defineProperty(internals.ReactCurrentDispatcher, "current", {
+	get() {
+		return currentDispatcher;
+	},
+	set(api) {
+		currentDispatcher = api;
+		if (lock) return;
+		if (api && !isInvalidHookAccessor(api)) {
+			// prevent re-injecting useReducer when the Dispatcher
+			// context changes to run the reducer callback:
+			lock = true;
+			const rerender = api.useReducer(UPDATE, {})[1];
+			lock = false;
+			const currentOwner = internals.ReactCurrentOwner.current;
+			let updater = updaterForComponent.get(currentOwner);
+			if (!updater) {
+				updater = createUpdater(rerender);
+				updaterForComponent.set(currentOwner, updater);
+			}
+			setCurrentUpdater(updater);
+		} else {
+			setCurrentUpdater();
+		}
+	},
+});
+
+// We inject a useReducer into every function component via CurrentDispatcher.
+// This prevents injecting into anything other than a function component render.
+const invalidHookAccessors = new Map();
+function isInvalidHookAccessor(api: ReactDispatcher) {
+	const cached = invalidHookAccessors.get(api);
+	if (cached !== undefined) return cached;
+	// we only want the real implementation, not the warning ones
+	const invalid =
+		api.useCallback.length < 2 ||
+		/warnInvalidHookAccess/.test(api.useCallback as any);
+	invalidHookAccessors.set(api, invalid);
+	return invalid;
+}
+
+export function useSignal<T>(value: T) {
+	return useMemo(() => signal<T>(value), []);
+}
+
+export function useComputed<T>(compute: () => T) {
+	const $compute = useRef(compute);
+	$compute.current = compute;
+	return useMemo(() => computed<T>(() => $compute.current()), []);
+}
---
+++ packages/react/test/index.test.ts
@@ -0,0 +1,162 @@
+// @ts-ignore-next-line
+globalThis.IS_REACT_ACT_ENVIRONMENT = true;
+
+import { signal, useComputed } from "@preact/signals-react";
+import { createElement as h, useMemo } from "react";
+import { createRoot, Root } from "react-dom/client";
+import { act } from "react-dom/test-utils";
+
+describe.only("@preact/signals-react", () => {
+	let scratch: HTMLDivElement;
+	let root: Root;
+	function render(element: Parameters<Root["render"]>[0]) {
+		act(() => root.render(element));
+	}
+
+	beforeEach(() => {
+		scratch = document.createElement("div");
+		root = createRoot(scratch);
+	});
+
+	afterEach(() => {
+		act(() => root.unmount());
+	});
+
+	describe("Text bindings", () => {
+		it("should render text without signals", () => {
+			render(h("span", null, "test"));
+			const span = scratch.firstChild;
+			const text = span?.firstChild;
+			expect(text).to.have.property("data", "test");
+		});
+
+		it("should render Signals as Text", () => {
+			const sig = signal("test");
+			render(h("span", null, sig));
+			const span = scratch.firstChild;
+			expect(span).to.have.property("firstChild").that.is.an.instanceOf(Text);
+			const text = span?.firstChild;
+			expect(text).to.have.property("data", "test");
+		});
+
+		it("should update Signal-based Text (no parent component)", () => {
+			const sig = signal("test");
+			render(h("span", null, sig));
+
+			const text = scratch.firstChild!.firstChild!;
+			expect(text).to.have.property("data", "test");
+
+			act(() => {
+				sig.value = "changed";
+			});
+
+			// should not remount/replace Text
+			expect(scratch.firstChild!.firstChild!).to.equal(text);
+			// should update the text in-place
+			expect(text).to.have.property("data", "changed");
+		});
+
+		it("should update Signal-based Text (in a parent component)", () => {
+			const sig = signal("test");
+			function App({ x }: { x: typeof sig }) {
+				return h("span", null, x);
+			}
+			render(h(App, { x: sig }));
+
+			const text = scratch.firstChild!.firstChild!;
+			expect(text).to.have.property("data", "test");
+
+			act(() => {
+				sig.value = "changed";
+			});
+
+			// should not remount/replace Text
+			expect(scratch.firstChild!.firstChild!).to.equal(text);
+			// should update the text in-place
+			expect(text).to.have.property("data", "changed");
+		});
+	});
+
+	describe("Component bindings", () => {
+		it("should subscribe to signals", () => {
+			const sig = signal("foo");
+
+			function App() {
+				const value = sig.value;
+				return h("p", null, value);
+			}
+
+			render(h(App, {}));
+			expect(scratch.textContent).to.equal("foo");
+
+			act(() => {
+				sig.value = "bar";
+			});
+			expect(scratch.textContent).to.equal("bar");
+		});
+
+		it("should activate signal accessed in render", () => {
+			const sig = signal(null);
+
+			function App() {
+				const arr = useComputed(() => {
+					// trigger read
+					sig.value;
+
+					return [];
+				});
+
+				const str = arr.value.join(", ");
+				return h("p", null, str);
+			}
+
+			const fn = () => render(h(App, {}));
+			expect(fn).not.to.throw;
+		});
+
+		it("should not subscribe to child signals", () => {
+			const sig = signal("foo");
+
+			function Child() {
+				const value = sig.value;
+				return h("p", null, value);
+			}
+
+			const spy = sinon.spy();
+			function App() {
+				spy();
+				return h(Child, null);
+			}
+
+			render(h(App, {}));
+			expect(scratch.textContent).to.equal("foo");
+
+			act(() => {
+				sig.value = "bar";
+			});
+			expect(spy).to.be.calledOnce;
+		});
+
+		it("should update memo'ed component via signals", async () => {
+			const sig = signal("foo");
+
+			function Inner() {
+				const value = sig.value;
+				return h("p", null, value);
+			}
+
+			function App() {
+				sig.value;
+				return useMemo(() => h(Inner, { foo: 1 }), []);
+			}
+
+			render(h(App, {}));
+			expect(scratch.textContent).to.equal("foo");
+
+			act(() => {
+				sig.value = "bar";
+			});
+			expect(scratch.textContent).to.equal("bar");
+		});
+	});
+});
