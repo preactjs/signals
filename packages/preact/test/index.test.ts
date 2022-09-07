01c95be57bc669178d517ee8a32f3900196b30e0
Jovi De Croock <decroockjovi@gmail.com>
2022-09-07 10:25:46 +0200

    add test for .value

--- packages/preact/test/index.test.ts
+++ packages/preact/test/index.test.ts
@@ -200,5 +200,24 @@ describe("@preact/signals", () => {
 			expect((scratch.firstChild?.childNodes[0] as HTMLInputElement).value).to.equal('bar')
 			expect((scratch.firstChild?.childNodes[1] as HTMLInputElement).value).to.equal('bar')
 		})
+
+		it('supports reusing an attribute value-signal', () => {
+			const s = signal('foo');
+			function App() {
+				// @ts-ignore
+				return h('div', {}, [h('input', { value: s.value }), h('input', { value: s.value })])
+			}
+			render(h(App, {}), scratch);
+
+			expect((scratch.firstChild?.childNodes[0] as HTMLInputElement).value).to.equal('foo')
+			expect((scratch.firstChild?.childNodes[1] as HTMLInputElement).value).to.equal('foo')
+			expect(s.peek()).to.equal('foo')
+
+			s.value = 'bar'
+			rerender();
+			expect(s.peek()).to.equal('bar')
+			expect((scratch.firstChild?.childNodes[0] as HTMLInputElement).value).to.equal('bar')
+			expect((scratch.firstChild?.childNodes[1] as HTMLInputElement).value).to.equal('bar')
+		})
 	})
 });
