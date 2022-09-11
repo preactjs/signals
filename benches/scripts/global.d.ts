16eebca4844af0c1983e7f18f6e361b371f7f83a
Jason Miller <jason@developit.ca>
2022-09-11 13:59:29 -0400

    add tachometer config from preact

---
+++ benches/scripts/global.d.ts
@@ -0,0 +1,16 @@
+interface TachometerOptions {
+	browser: string | string[];
+	framework: string | string[];
+	"window-size": string;
+	"sample-size": number;
+	horizon: string;
+	timeout: number;
+	trace: boolean;
+	memory: boolean;
+}
+
+interface DeoptOptions {
+	framework: string;
+	timeout: number;
+	open: boolean;
+}
