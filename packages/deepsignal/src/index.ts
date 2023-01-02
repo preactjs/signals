458cdc85e3570fcb80665033b44a2969b4a6fc5d
Luis Herranz <luisherranz@gmail.com>
2023-01-02 18:13:46 +0100

    Add arrays of signals and $$

--- packages/deepsignal/src/index.ts
+++ packages/deepsignal/src/index.ts
@@ -2,10 +2,13 @@ import { signal, Signal } from "@preact/signals-core";
 
 const proxyToSignals = new WeakMap();
 const objToProxy = new WeakMap();
-const rg = /^\$/;
+const arrayToArrayOfSignals = new WeakMap();
+const rg = /^\$\$?/;
 
 type DeepSignalObject<T extends object> = {
 	[P in keyof T & string as `$${P}`]?: Signal<T[P]>;
+} & {
+	[P in keyof T & string as `$$${P}`]?: T[P];
 } & {
 	[P in keyof T]: T[P] extends Array<unknown>
 		? DeepSignalArray<T[P]>
@@ -14,12 +17,13 @@ type DeepSignalObject<T extends object> = {
 		: T[P];
 };
 
-type ArrayType<T> = T extends Array<infer Item> ? Item : T;
+type ArrayType<T> = T extends Array<infer I> ? I : T;
 type DeepSignalArray<T> = Array<ArrayType<T>> & {
 	[key: number]: DeepSignal<ArrayType<T>>;
-	[key: `$${number}`]: Signal<ArrayType<T>>;
-	length: number;
+	$?: { [key: number]: Signal<ArrayType<T>> };
+	$$?: { [key: number]: ArrayType<T> };
 	$length?: Signal<number>;
+	$$length?: number;
 };
 
 type DeepSignal<T> = T extends Array<unknown>
@@ -29,31 +33,44 @@ type DeepSignal<T> = T extends Array<unknown>
 	: T;
 
 export const deepSignal = <T extends object>(obj: T): DeepSignal<T> => {
-	return new Proxy(obj, handlers) as DeepSignal<T>;
+	return new Proxy(obj, objectHandlers) as DeepSignal<T>;
 };
 
-const handlers = {
-	get(target: object, originalKey: string, receiver: object) {
-		const returnSignal = originalKey[0] === "$";
-		const key = returnSignal ? originalKey.replace(rg, "") : originalKey;
+const get =
+	(isArrayOfSignals: boolean) =>
+	(target: object, fullKey: string, receiver: object): unknown => {
+		let returnSignal = isArrayOfSignals || fullKey[0] === "$";
+		if (!isArrayOfSignals && returnSignal && Array.isArray(target)) {
+			if (fullKey === "$") {
+				if (!arrayToArrayOfSignals.has(target))
+					arrayToArrayOfSignals.set(target, new Proxy(target, arrayHandlers));
+				return arrayToArrayOfSignals.get(target);
+			} else if (fullKey === "$$") return target;
+			else returnSignal = fullKey === "$length" || fullKey === "$$length";
+		}
+		const key = returnSignal ? fullKey.replace(rg, "") : fullKey;
 		let value = Reflect.get(target, key, receiver);
+		if (returnSignal && fullKey[1] === "$") return value;
 		if (!proxyToSignals.has(receiver)) proxyToSignals.set(receiver, new Map());
 		const signals = proxyToSignals.get(receiver);
 		if (!signals.has(key)) {
 			if (typeof value === "object" && value !== null) {
 				if (!objToProxy.has(value))
-					objToProxy.set(value, new Proxy(value, handlers));
+					objToProxy.set(value, new Proxy(value, objectHandlers));
 				value = objToProxy.get(value);
 			}
 			signals.set(key, signal(value));
 		}
 		return returnSignal ? signals.get(key) : signals.get(key).value;
-	},
+	};
 
+const objectHandlers = {
+	get: get(false),
 	set(target: object, key: string, val: any, receiver: object) {
 		let internal = val;
 		if (typeof val === "object" && val !== null) {
-			if (!objToProxy.has(val)) objToProxy.set(val, new Proxy(val, handlers));
+			if (!objToProxy.has(val))
+				objToProxy.set(val, new Proxy(val, objectHandlers));
 			internal = objToProxy.get(val);
 		}
 		if (!proxyToSignals.has(receiver)) proxyToSignals.set(receiver, new Map());
@@ -66,3 +83,5 @@ const handlers = {
 		return result;
 	},
 };
+
+const arrayHandlers = { get: get(true) };
--- packages/deepsignal/test/index.test.tsx
+++ packages/deepsignal/test/index.test.tsx
@@ -5,88 +5,107 @@ import { setupRerender, act } from "preact/test-utils";
 
 const sleep = (ms?: number) => new Promise(r => setTimeout(r, ms));
 
-describe("deepsignal", () => {
+describe.only("deepsignal", () => {
 	let scratch: HTMLDivElement;
 	let rerender: () => void;
+	let nested = { b: 2 };
+	let array = [3, nested];
+	let v = { a: 1, nested, array };
+	let s = deepSignal(v);
 
 	beforeEach(() => {
 		scratch = document.createElement("div");
 		rerender = setupRerender();
+		nested = { b: 2 };
+		array = [3, nested];
+		v = { a: 1, nested, array };
+		s = deepSignal(v);
 	});
 
 	afterEach(() => {
 		render(null, scratch);
 	});
 
-	it("should return signal value by default", () => {
-		const nested = { b: 2 };
-		const array = [3, nested];
-		const v = { a: 1, nested, array };
-		const s = deepSignal(v);
-		const a = deepSignal(["a"]);
-		a.$length;
-		a.length;
+	it.only("should return like plain objects/arrays", () => {
 		expect(s.a).to.equal(1);
-		expect(s.nested).to.have.property("b", 2);
 		expect(s.nested.b).to.equal(2);
 		expect(s.array[0]).to.equal(3);
-		expect(s.array[1]).to.have.property("b", 2);
-		expect(s.array[1].b).to.equal(2);
+		expect(typeof s.array[1] === "object" && s.array[1].b).to.equal(2);
 		expect(s.array.length).to.equal(2);
 	});
 
-	it("should return signal instance when using $", () => {
-		const nested = { b: 2 };
-		const array = [3, nested];
-		const v = { a: 1, nested, array };
-		const s = deepSignal(v);
+	it.only("should update like plain objects/arrays", () => {
+		expect(s.a).to.equal(1);
+		expect(s.nested.b).to.equal(2);
+		s.a = 2;
+		s.nested.b = 3;
+		expect(s.a).to.equal(2);
+		expect(s.nested.b).to.equal(3);
+	});
+
+	it.only("should update array length", () => {
+		expect(s.array.length).to.equal(2);
+		s.array.push(4);
+		expect(s.array.length).to.equal(3);
+		s.array.splice(1, 2);
+		expect(s.array.length).to.equal(1);
+	});
+
+	it.only("should return signal instance when using $", () => {
 		expect(s.$a).to.be.instanceOf(Signal);
 		expect(s.$a!.value).to.equal(1);
 		expect(s.$nested).to.be.instanceOf(Signal);
-		expect(s.$nested!.value).to.have.property("b", 2);
+		expect(s.$nested!.value.b).to.equal(2);
 		expect(s.nested.$b).to.be.instanceOf(Signal);
 		expect(s.nested.$b!.value).to.equal(2);
 		expect(s.$array).to.be.instanceOf(Signal);
-		expect(s.$array!.value).to.have.property("0", 3);
-		expect(s.array.$0).to.be.instanceOf(Signal);
-		expect(s.array.$0.value).to.equal(3);
-		expect(s.array.$1).to.be.instanceOf(Signal);
-		expect(s.array.$1.value).to.have.property("b", 2);
-		expect(s.array[1].$b).to.be.instanceOf(Signal);
-		expect(s.array[1].$b.value).to.equal(2);
+		expect(s.$array!.value[0]).to.equal(3);
+		expect(s.array.$![0]).to.be.instanceOf(Signal);
+		expect(s.array.$![0].value).to.equal(3);
+		expect(s.array.$![1]).to.be.instanceOf(Signal);
+		expect(
+			typeof s.array.$![1].value === "object" && s.array.$![1].value.b
+		).to.equal(2);
+		expect(typeof s.array[1] === "object" && s.array[1].$b).to.be.instanceOf(
+			Signal
+		);
+		expect(typeof s.array[1] === "object" && s.array[1].$b!.value).to.equal(2);
 		expect(s.array.$length).to.be.instanceOf(Signal);
-		expect(s.array.$length.value).to.equal(2);
+		expect(s.array.$length!.value).to.equal(2);
 	});
 
-	it("signal should return peek (value)", () => {
-		const nested = { b: 2 };
-		const array = [3, nested];
-		const v = { a: 1, nested, array };
-		const s = deepSignal(v);
-		expect(s.$a!.peek()).to.equal(1);
-		expect(s.$nested!.peek()).to.have.property("b", 2);
-		expect(s.$nested!.peek().b).to.equal(2);
-		expect(s.nested.$b!.peek()).to.equal(2);
-		expect(s.$array!.peek()).to.have.property("0", 3);
-		expect(s.$array!.peek()[0]).to.equal(3);
-		expect(s.$array!.peek()[1]).to.have.property("b", 2);
-		expect(s.array.$1!.peek().b).to.equal(2);
-		expect(s.array[1].$b!.peek()).to.equal(2);
-		expect(s.$array!.peek().length).to.equal(2);
-		expect(s.array.$length!.peek()).to.equal(2);
+	it.only("should return peek when using $$", () => {
+		expect(s.$$a).to.equal(1);
+		expect(s.$$nested!.b).to.equal(2);
+		expect(s.nested.$$b).to.equal(2);
+		expect(s.$$array![0]).to.equal(3);
+		expect(s.array.$$![0]).to.equal(3);
+		expect(typeof s.$$array![1] === "object" && s.$$array![1].b).to.equal(2);
+		expect(typeof s.array.$$![1] === "object" && s.array.$$![1].b).to.equal(2);
+		expect(s.$$array!.length).to.equal(2);
+		expect(s.array.$$length).to.equal(2);
 	});
 
-	it("should subscribe to changes", () => {
-		const nested = { b: 2 };
-		const array = [3, nested];
-		const v = { a: 1, nested, array };
-		const s = deepSignal(v);
+	it.only("should update array $length", () => {
+		expect(s.array.$length!.value).to.equal(2);
+		s.array.push(4);
+		expect(s.array.$length!.value).to.equal(3);
+		s.array.splice(1, 2);
+		expect(s.array.$length!.value).to.equal(1);
+	});
 
+	it.only("should not return signals in plain arrays using $prop", () => {
+		expect((s.array as any).$0).to.be.undefined;
+	});
+
+	it.only("should subscribe to changes", () => {
 		const spy1 = sinon.spy(() => s.a);
 		const spy2 = sinon.spy(() => s.nested);
 		const spy3 = sinon.spy(() => s.nested.b);
 		const spy4 = sinon.spy(() => s.array[0]);
-		const spy5 = sinon.spy(() => s.array[1].b);
+		const spy5 = sinon.spy(
+			() => typeof s.array[1] === "object" && s.array[1].b
+		);
 
 		effect(spy1);
 		effect(spy2);
@@ -132,7 +151,7 @@ describe("deepsignal", () => {
 		expect(spy4).callCount(2);
 		expect(spy5).callCount(2);
 
-		s.array[1].b = 2222;
+		if (typeof s.array[1] === "object") s.array[1].b = 2222;
 
 		expect(spy1).callCount(2);
 		expect(spy2).callCount(2);
@@ -173,7 +192,7 @@ describe("deepsignal", () => {
 		expect(spy5).callCount(5);
 	});
 
-	it("should subscribe to array length", () => {
+	it.only("should subscribe to array length", () => {
 		const array = [1];
 		const s = deepSignal({ array });
 		const spy1 = sinon.spy(() => s.array.length);
@@ -201,17 +220,15 @@ describe("deepsignal", () => {
 	});
 
 	it.only("should not subscribe to changes when peeking", () => {
-		const nested = { b: 2 };
-		const array = [3, nested];
-		const v = { a: 1, nested, array };
-		const s = deepSignal(v);
-
 		const spy1 = sinon.spy(() => s.$$a);
 		const spy2 = sinon.spy(() => s.$$nested);
-		const spy3 = sinon.spy(() => s.$$nested.b);
-		const spy4 = sinon.spy(() => s.$$array[0]);
-		const spy5 = sinon.spy(() => s.$$array[1].b);
-		const spy6 = sinon.spy(() => s.$$array.length);
+		const spy3 = sinon.spy(() => s.$$nested!.b);
+		const spy4 = sinon.spy(() => s.$$array![0]);
+		const spy5 = sinon.spy(() => {
+			const nested = s.array.$$![1];
+			if (typeof nested === "object") nested.b;
+		});
+		const spy6 = sinon.spy(() => s.array.$$length);
 
 		effect(spy1);
 		effect(spy2);
@@ -231,7 +248,7 @@ describe("deepsignal", () => {
 		s.nested.b = 22;
 		s.nested = { b: 222 };
 		s.array[0] = 33;
-		s.array[1].b = 2222;
+		if (typeof s.array[1] === "object") s.array[1].b = 2222;
 		s.array.push(4);
 
 		expect(spy1).callCount(1);
@@ -240,9 +257,17 @@ describe("deepsignal", () => {
 		expect(spy4).callCount(1);
 		expect(spy5).callCount(1);
 		expect(spy6).callCount(1);
+
+		const spy7 = sinon.spy(() => s.nested.$$b);
+		effect(spy7);
+		expect(spy7).callCount(1);
+		s.nested.b = 22;
+		expect(spy7).callCount(1);
+		s.nested = { b: 222 };
+		expect(spy7).callCount(2);
 	});
 
-	it("should respect object references", () => {
+	it.skip("should preserve object references", () => {
 		const nested = { b: 2 };
 		const array = [3, nested];
 		const obj = { a: 1, nested, array };
