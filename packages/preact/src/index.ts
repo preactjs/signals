91409600f28578f9a2859bc7b8339a7a943fd734
XantreGodlike <valerik007.vs@gmail.com>
2023-07-15 12:28:14 +0300

    added more explicit jsx checking

--- packages/preact/src/index.ts
+++ packages/preact/src/index.ts
@@ -1,4 +1,4 @@
-import { options, Component } from "preact";
+import { options, Component, isValidElement } from "preact";
 import { useRef, useMemo, useEffect } from "preact/hooks";
 import {
 	signal,
@@ -83,19 +83,18 @@ function Text(this: AugmentedComponent, { data }: { data: Signal }) {
 		const defaultUpdaterCallback = this._updater!._callback;
 
 		// Replace this component's vdom updater with a direct text one:
-		const stringOrJSXUpdaterCallback = () => {
+		this._updater!._callback = () => {
 			const node = this.base;
 			// if we need to update the text directly, we should ensure that current node is a text node
 			// if it is not, we should promise preact to create it via rerendering the whole component
-			if (typeof s.peek() === "object" || node?.nodeType !== 3) {
+			if (isValidElement(s.peek()) || node?.nodeType !== 3) {
 				defaultUpdaterCallback();
 
 				return;
 			}
 
-			(this.base as Text).data = s.peek();
+			(node as Text).data = s.peek();
 		};
-		this._updater!._callback = stringOrJSXUpdaterCallback;
 
 		return computed(() => {
 			let data = currentSignal.value;
