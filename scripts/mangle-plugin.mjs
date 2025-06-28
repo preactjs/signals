f12b8ef676bc2db2083d5fb60dc484dc3c4f6876
James Garbutt <43081j@users.noreply.github.com>
2025-06-28 20:29:24 +0100

    test: add vitest prep
    Adds vitest and associated scripts which will not succeed yet, as we
    have no projects.

---
+++ scripts/mangle-plugin.mjs
@@ -0,0 +1,44 @@
+import { readFileSync } from 'node:fs';
+import { transformAsync } from '@babel/core';
+const rename = {};
+const mangle = readFileSync('./mangle.json', 'utf8');
+const mangleJson = JSON.parse(mangle);
+for (let prop in mangleJson.props.props) {
+	let name = prop;
+	if (name[0] === '$') {
+		name = name.slice(1);
+	}
+
+	rename[name] = mangleJson.props.props[prop];
+}
+export const manglePlugin = {
+	name: 'rename-mangle-properties',
+	async transform(code, id) {
+		if (id.includes('node_modules')) {
+			return null;
+		}
+
+		const shouldTransform = id.includes('src') || id.includes('test');
+		if (!shouldTransform) {
+			return null;
+		}
+
+		const transformed = await transformAsync(code, {
+			filename: id,
+			configFile: false,
+			plugins: [
+				[
+					'babel-plugin-transform-rename-properties',
+					{
+						rename
+					}
+				]
+			]
+		});
+
+		return {
+			code: transformed.code,
+			map: transformed.map
+		};
+	}
+};
