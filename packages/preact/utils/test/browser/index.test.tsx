bee01e56140c2e6dd5aabd27b2cdcb0043a89d4d
Jovi De Croock <decroockjovi@gmail.com>
2025-10-27 08:08:07 +0100

    Account for nested reactivity in For

--- packages/preact/utils/test/browser/index.test.tsx
+++ packages/preact/utils/test/browser/index.test.tsx
@@ -1,4 +1,4 @@
-import { signal } from "@preact/signals";
+import { computed, signal } from "@preact/signals";
 import { For, Show, useSignalRef } from "@preact/signals/utils";
 import { render, createElement } from "preact";
 import { act } from "preact/test-utils";
@@ -34,6 +34,31 @@ describe("@preact/signals-utils", () => {
 			});
 			expect(scratch.innerHTML).to.eq("<p>Showing</p>");
 		});
+
+		it("Should reactively show an inline element w/ nested reactivity", () => {
+			const count = signal(0);
+			const visible = computed(() => count.value > 0)!;
+			const Paragraph = (props: any) => <p>{props.children}</p>;
+			act(() => {
+				render(
+					<Show when={visible} fallback={<Paragraph>Hiding</Paragraph>}>
+						<Paragraph>Showing {count}</Paragraph>
+					</Show>,
+					scratch
+				);
+			});
+			expect(scratch.innerHTML).to.eq("<p>Hiding</p>");
+
+			act(() => {
+				count.value = 1;
+			});
+			expect(scratch.innerHTML).to.eq("<p>Showing 1</p>");
+
+			act(() => {
+				count.value = 2;
+			});
+			expect(scratch.innerHTML).to.eq("<p>Showing 2</p>");
+		});
 	});
 
 	describe("<For />", () => {
@@ -54,6 +79,42 @@ describe("@preact/signals-utils", () => {
 				list.value = ["foo", "bar"];
 			});
 			expect(scratch.innerHTML).to.eq("<p>foo</p><p>bar</p>");
+		});
+
+		it("Should iterate over a list of signals w/ nested reactivity", () => {
+			const list = signal<Array<string>>([])!;
+			const test = signal("foo");
+			const Paragraph = (p: any) => <p>{p.children}</p>;
+			act(() => {
+				render(
+					<For each={list} fallback={<Paragraph>No items</Paragraph>}>
+						{item => (
+							<Paragraph key={item}>
+								{item}-{test.value}
+							</Paragraph>
+						)}
+					</For>,
+					scratch
+				);
+			});
+			expect(scratch.innerHTML).to.eq("<p>No items</p>");
+
+			act(() => {
+				list.value = ["foo", "bar"];
+			});
+			expect(scratch.innerHTML).to.eq("<p>foo-foo</p><p>bar-foo</p>");
+
+			act(() => {
+				test.value = "baz";
+			});
+			expect(scratch.innerHTML).to.eq("<p>foo-baz</p><p>bar-baz</p>");
+
+			act(() => {
+				list.value = ["foo", "bar", "qux"];
+			});
+			expect(scratch.innerHTML).to.eq(
+				"<p>foo-baz</p><p>bar-baz</p><p>qux-baz</p>"
+			);
 		});
 	});
 
