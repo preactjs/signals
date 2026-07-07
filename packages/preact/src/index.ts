89000a24d02bb352e2a1a2dcb971edda413184f4
Jovi De Croock <decroockjovi@gmail.com>
2026-07-07 12:12:32 +0200

    Remove seeing useContext as HOOKS_STATE (#952)

--- packages/preact/src/index.ts
+++ packages/preact/src/index.ts
@@ -389,7 +389,7 @@ hook(OptionsTypes.UNMOUNT, (old, vnode: VNode) => {
 
 /** Mark components that use hook state so we can skip sCU optimization. */
 hook(OptionsTypes.HOOK, (old, component, index, type) => {
-	if (type < 3 || type === 9)
+	if (type < 3)
 		(component as AugmentedComponent)._updateFlags |= HAS_HOOK_STATE;
 	old(component, index, type);
 });
